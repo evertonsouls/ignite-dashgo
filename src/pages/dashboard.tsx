513180f0311a149c0401b3698097ad49035bd175
Everton Pereira <evertonsouls@gmail.com>
2021-04-25 23:01:29 +0200

    first commit

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,24 @@
+import { Flex } from '@chakra-ui/react'
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+
+export default function Dashbord() {
+  return (
+    <Flex 
+      direction="column"
+      h="100vh"
+    >
+      <Header />
+
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+      >
+        <Sidebar />
+      </Flex>
+    </Flex>
+  )
+}
